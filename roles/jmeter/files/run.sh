d3dec5fc36795678d5ec7458115cfb49c67b7fcb
LEE SO JUNG <90453158+xxoznge@users.noreply.github.com>
2025-04-07 11:47:51 +0900

    Refactor: jMeter 시나리오 수정 (#50)

--- roles/jmeter/files/run.sh
+++ roles/jmeter/files/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export HEAP="-Xms128m -Xmx256m"
+
+# report 디렉터리 있으면 삭제
+[ -d /opt/jmeter/report ] && rm -rf /opt/jmeter/report
+
 /opt/jmeter/bin/jmeter -n \
   -t /opt/jmeter/noHands.jmx \
   -l /opt/jmeter/result.jtl \
